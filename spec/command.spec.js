d0bc6622eb9727bff65a3cc373c70914782e1e4a
stephenmdevine <stephenmdevine@gmail.com>
2023-10-11 22:56:04 -0500

    Task 2: Command Class complete, with 3 tests passing

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,15 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  
+  test("constructor sets command type", () => {
+    let expected = new Command(" ");
+    expect(expected.commandType).toEqual(" ");
+  });
+
+  test("constructor sets a value passed in as the 2nd argument", () => {
+    let expected = new Command(" ", 0);
+    expect(expected.value).toEqual(0);
+  });
 
 });
